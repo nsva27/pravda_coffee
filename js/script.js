ff51fd0af7bb101db60a719f411bb961732a0fa1
Valery <nesterovsky.valery@gmail.com>
2023-04-14 14:18:58 +0300

    made menu-list section

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header'),
           navBar = document.querySelector('.header__wrapper'),
           headerLogoImage = document.querySelector('.header__logo-img'),
-          mainSection = document.querySelector('.main');
+          mainSection = document.querySelector('.main'),
+          menuHeader = document.querySelector('.menu');
           
 
     window.addEventListener('resize', hideNavbar);
@@ -15,14 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
             navBar.classList.add('header__wrapper_short');
             headerLogoImage.setAttribute('src', 'img/logo_white_symbol.png');
             header.classList.add('header_short');
+            menuHeader.classList.add('menu_shaddow');
         } else if (document.documentElement.scrollTop > mainSection.offsetHeight - 80 && navBar.offsetWidth < 960){
             navBar.classList.add('header__wrapper_short');
             headerLogoImage.setAttribute('src', 'img/logo_white_symbol.png');
             header.classList.add('header_short');
+            menuHeader.classList.add('menu_shaddow');
         } else {
             navBar.classList.remove('header__wrapper_short');
             headerLogoImage.setAttribute('src', 'img/logo_white.png');
             header.classList.remove('header_short');
+            menuHeader.classList.remove('menu_shaddow');
         } 
     }
 
